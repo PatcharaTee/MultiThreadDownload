b739544b7477b0f9caea0be85230e72e9f3d6c83
PatcharaTee <deedeebaker8@gmail.com>
2021-03-15 13:42:09 +0700

    upload project

---
+++ Client.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MultiThreadDownloadFile;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class Client {
+
+    public static void main(String[] args) throws IOException, InterruptedException, ExecutionException {
+        //load server config
+        ClientConfig ccf = new ClientConfig();
+        //Set up - Client can set where to save file, server and port at class ClientConfig
+        int port = ccf.getPort();
+        String server = ccf.getServer();
+        File clientFolder = new File(ccf.getClientFolder());
+        Scanner scan = new Scanner(System.in);
+
+        //Try to connect to server and Wait for user interaction
+        try {
+            Socket clientSock = new Socket(server, port);
+            if (clientSock.isConnected()) {
+                System.out.println("Connected to server.");
+            }
+            //Data i/o for communication with server
+            DataInputStream dis = new DataInputStream(clientSock.getInputStream());
+            DataOutputStream dos = new DataOutputStream(clientSock.getOutputStream());
+            OUTER:
+            while (true) {
+                System.out.print("Type Files to get file list.\n"
+                        + "     Download to download file.\n"
+                        + "     Exit to disconnect.\n"
+                        + "Input = ");
+                //Send command to server
+                String toSend = scan.nextLine();
+                String recive;
+                dos.writeUTF(toSend);
+                switch (toSend) {
+                    case "Files":
+                        System.out.println("- File list -");
+                        int totalFiles = dis.read();
+                        for (int i = 1; i <= totalFiles; i++) {
+                            System.out.println(i + " " + dis.readUTF());
+                        }
+                        //Recive message
+                        recive = dis.readUTF();
+                        System.out.println(recive + "\n");
+                        break;
+                    case "Download":
+                        //Recive input from user and sent to filename to server
+                        System.out.print("File name = ");
+                        String fileName = scan.nextLine();
+                        dos.writeUTF(fileName);
+                        //Get file size from server
+                        int fileSize = dis.readInt();
+                        if (fileSize > 0) {
+                            System.out.println("Downloading...");
+                            int partSize = fileSize / 10;
+                            FileOutputStream fos = new FileOutputStream(clientFolder + "/" + fileName, true);
+                            ExecutorService executor = Executors.newFixedThreadPool(10);
+                            for (int i = 0; i < 10; i++) {
+                                int startIndex = i * partSize;
+                                int endIndex = ((i + 1) * partSize) - 1;
+                                if (i == 9) {
+                                    endIndex = fileSize;
+                                }
+                                System.out.println("Part " + i + ", StartIndex = " + startIndex + ", EndIndex = " + endIndex);
+                                Future<byte[]> dPart = executor.submit(new Download(fileName, startIndex, endIndex));
+                                fos.write(dPart.get());
+                            }
+                            executor.shutdown();
+                            fos.close();
+                            //Recive message
+                            recive = dis.readUTF();
+                            System.out.println(recive + "\n");
+                        } else {
+                            //Recive message
+                            recive = dis.readUTF();
+                            System.out.println(recive + " \n");
+                        }
+                        break;
+                    case "Exit":
+                        //Recive message
+                        recive = dis.readUTF();
+                        System.out.println(recive + " ");
+                        //Close connection and end loop
+                        clientSock.close();
+                        break OUTER;
+                    default:
+                        //Recive message
+                        recive = dis.readUTF();
+                        System.out.println(recive + "\n");
+                        break;
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println("Can't connect to the Server.");
+            //System.out.println(ex);
+            System.exit(0);
+        }
+    }
+}
+
+class ClientConfig {
+
+    private final int port = 21;
+    private final String server = "127.0.0.1";
+    private final String clientFolder = "C:/Users/Admin/Desktop/APP/FTPTestPath/ClientFolder";
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getServer() {
+        return server;
+
+    }
+
+    public String getClientFolder() {
+        return clientFolder;
+    }
+}
---
+++ Server.java
@@ -0,0 +1,170 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MultiThreadDownloadFile;
+
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
+
+/*
+import java.util.logging.Level;
+import java.util.logging.Logger;
+ */
+/**
+ *
+ * @author Admin
+ */
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        //load server config
+        ServerConfig scf = new ServerConfig();
+        //Set up - Admin can set where client to read file and port at class ServerConfig
+        int port = scf.getPort();
+        ServerSocket serverSock = new ServerSocket(port);
+        System.out.println("Server started.");
+
+        //Wait for connection
+        while (true) {
+            try {
+                Socket clientSock = serverSock.accept();
+                System.out.println("Client accept. " + clientSock);
+                //Data i/o for communication with client
+                DataInputStream dis = new DataInputStream(clientSock.getInputStream());
+                DataOutputStream dos = new DataOutputStream(clientSock.getOutputStream());
+                //New client thread
+                ClientHandler handler = new ClientHandler(clientSock, dis, dos);
+                handler.start();
+            } catch (IOException ex) {
+                //System.out.println(ex);
+            }
+        }
+    }
+}
+
+class ServerConfig {
+
+    private final int port = 21;
+    private final String serverFolder = "C:/Users/Admin/Desktop/APP/FTPTestPath/ServerFolder";
+
+    public String getServerFolder() {
+        return serverFolder;
+    }
+
+    public int getPort() {
+        return port;
+    }
+}
+
+class ClientHandler extends Thread {
+
+    //load server config
+    ServerConfig scf = new ServerConfig();
+    //var
+    File serverFolder = new File(scf.getServerFolder());
+    Socket clientSock;
+    DataInputStream dis;
+    DataOutputStream dos;
+
+    public ClientHandler(Socket clientSock, DataInputStream dis, DataOutputStream dos) {
+        this.clientSock = clientSock;
+        this.dis = dis;
+        this.dos = dos;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Thread start for Client " + clientSock);
+        File[] files = serverFolder.listFiles();
+        while (true) {
+            try {
+                String recive = dis.readUTF();
+                String toSend;
+                switch (recive) {
+                    case "Files":
+                        //Return message
+                        int totalFiles = files.length;
+                        dos.write(totalFiles);
+                        //Sent all file name to client to print
+                        for (File file : files) {
+                            String filename = file.getName();
+                            dos.writeUTF(filename);
+                        }
+                        //Return message
+                        toSend = "Finish tranfer.";
+                        dos.writeUTF(toSend);
+                        break;
+                    case "Download":
+                        //Recive file name from client
+                        String fileName = dis.readUTF();
+                        //Find lenght of file
+                        int fileSize = 0;
+                        for (File file : files) {
+                            if (fileName.equals(file.getName())) {
+                                fileSize = (int) file.length();
+                                break;
+                            }
+                        }
+                        //Return message (file size, path to file)
+                        dos.writeInt(fileSize);
+                        if (fileSize > 0) {
+                            //Server echo
+                            System.out.println("Client " + clientSock + " download request file " + fileName + ", Size = " + fileSize + " bytes, Start sending file.");
+                            //Return message
+                            toSend = "Finished tranfer file.";
+                            dos.writeUTF(toSend);
+                        } else {
+                            //Server echo
+                            System.out.println("Client " + clientSock + " download request file " + fileName + ", File not found.");
+                            //Return message
+                            toSend = "File not found.";
+                            dos.writeUTF(toSend);
+                        }
+                        break;
+                    case "Exit":
+                        //Server echo
+                        System.out.println("Client disconnect. " + clientSock);
+                        //Return message
+                        toSend = "Connection is closed, Goodbye.";
+                        dos.writeUTF(toSend);
+                        break;
+                    default:
+                        //Return message
+                        toSend = "Wrong Input.";
+                        dos.writeUTF(toSend);
+                }
+            } catch (IOException ex) {
+                //Logger.getLogger(ClientHandler.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
+
+class Download implements Callable<byte[]> {
+
+    //load server config
+    ServerConfig scf = new ServerConfig();
+    //var
+    int startIndex, endIndex;
+    String fileName, serverFolder = scf.getServerFolder();
+
+    public Download(String fileName, int startIndex, int endIndex) {
+        this.fileName = fileName;
+        this.startIndex = startIndex;
+        this.endIndex = endIndex;
+    }
+
+    @Override
+    public byte[] call() throws FileNotFoundException, IOException {
+        int len = (endIndex - startIndex) + 1;
+        byte[] dPart = new byte[len];
+        RandomAccessFile raf = new RandomAccessFile(serverFolder + "/" + fileName, "r");
+        raf.seek(startIndex);
+        raf.read(dPart, 0, len);
+        raf.close();
+        return dPart;
+    }
+}
